f7be3f877aa7f04708c600bde3cd9332c884d9bd
Kilarus97 <zecmirko97@gmail.com>
2025-09-17 15:05:49 +0200

    Integrisana Databaza sa postgres-om

---
+++ BookstoreApplication/BookstoreApplication/Data/BookstoreDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookstoreApplication.Models;
+
+namespace BookstoreApplication.Data
+{
+    public class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Author)
+                .WithMany()
+                .HasForeignKey(b => b.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)
+                .WithMany()
+                .HasForeignKey(b => b.PublisherId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- BookstoreApplication/BookstoreApplication/Program.cs
+++ BookstoreApplication/BookstoreApplication/Program.cs
@@ -1,23 +1,43 @@
+﻿using System;
+using BookstoreApplication.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Dodaj CORS servis pre nego što pozoveš builder.Build()
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
+// Ostale servise
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookstoreDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. Middleware pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting(); // obavezno pre CORS-a
+app.UseCors("AllowFrontend"); // sad je validan jer je servis već registrovan
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+
